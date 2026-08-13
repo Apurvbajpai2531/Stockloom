862d7684491faaa9d2598c0cb3f067d00aadc2d0
Apurvbajpai2531 <apurvbjp@gmail.com>
2026-08-13 15:38:28 +0530

    fixes maded

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -65,7 +65,7 @@ variable "node_max_size" {
 variable "node_desired_size" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_disk_size" {
