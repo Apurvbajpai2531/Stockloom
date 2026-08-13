6dc805603e43c0d91fe82b325c584a8c1f3c8dac
Apurvbajpai2531 <apurvbjp@gmail.com>
2026-08-13 18:32:22 +0530

    fix: upgrade node instance type to t3.medium for observability stack memory needs

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -47,7 +47,7 @@ variable "kubernetes_version" {
 variable "node_instance_type" {
   description = "EKS worker node instance type"
   type        = string
-  default     = "t3.small"
+  default     = "t3.medium"
 }
 
 variable "node_min_size" {
